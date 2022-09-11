dcb12b759733192ab5a5ed98cecce2ddf5891a2b
narutiga <100295602+narutiga@users.noreply.github.com>
2022-09-11 18:59:36 +0900

    ✨ Spinner作成

---
+++ src/component/Spinner/Spinner .tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+/** @package */
+export const Spinner: FC = () => {
+  return (
+    <div className="my-5 h-8 w-8 animate-spin rounded-full border-2 border-yellow-400 border-t-transparent"></div>
+  );
+};
