63b60948e521289125907e9ec062214a72b0bb40
narutiga <100295602+narutiga@users.noreply.github.com>
2022-09-11 09:21:20 +0900

    🚧 supabaseとtypeを追加

---
+++ src/util/type/index.ts
@@ -0,0 +1,1 @@
+export type { Fortune } from "./type";
---
+++ src/util/type/type.tsx
@@ -0,0 +1,8 @@
+/** @package */
+export type Fortune = {
+  id: string;
+  create_at: string;
+  date: Date;
+  title: string;
+  user_id: string | undefined;
+};
