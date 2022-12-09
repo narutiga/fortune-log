ba9e0aa661c4ef3d2860857e73f255902ae8c1c0
narutiga <100295602+narutiga@users.noreply.github.com>
2022-12-09 20:32:34 +0900

    ➕ Add Mantine

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
     "src/component/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screen: {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1400px",
+    },
     extend: {
       spacing: {
         22: "5.5rem",
