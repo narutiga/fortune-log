41f1dcb9a3cd06a5aec55da7074deb95957d1255
narutiga <100295602+narutiga@users.noreply.github.com>
2022-12-24 00:03:47 +0900

    ✨ Feature Timeline

--- src/pages/log.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-import { Log } from "src/pages-component/log";
-
-const LogPage: NextPage = (props) => {
-  return <Log {...props} />;
-};
-
-export default LogPage;
---
+++ src/pages/timeline.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { Timeline } from "src/pages-component/timeline";
+
+const TimelinePage: NextPage = (props) => {
+  return <Timeline {...props} />;
+};
+
+export default TimelinePage;
