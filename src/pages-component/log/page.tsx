c8970cf92e7ac4d73478ea828aaedf5afa2f7967
narutiga <100295602+narutiga@users.noreply.github.com>
2022-12-23 22:19:28 +0900

    🚧 Change logPage

--- src/pages-component/log/page.tsx
+++ src/pages-component/log/page.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
 import { Layout } from "src/component/Layout";
 import { FortuneList } from "src/component/FortuneList";
+import { useQueryFortunes } from "src/lib/hook/useQueryFortunes";
 
 /** @pakage */
 export const Log: FC = () => {
+  const { data: fortunes, status } = useQueryFortunes();
+
   return (
     <Layout>
-      <FortuneList />
+      <FortuneList fortunes={fortunes} status={status} />
     </Layout>
   );
 };
